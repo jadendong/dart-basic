30521e0a84d32c19830a706522f5c8cf3bc56fc5
Jaden Dong <app@jadenmail.com>
2019-03-31 01:16:19 +0800

    变量与常量

---
+++ ch2/var_and_const.dart
@@ -0,0 +1,22 @@
+// 变量与常量
+void main() {
+  var a;
+  print(a);
+
+  a = 10;
+  print(a);
+
+  a = 'Hello Dart!';
+  print(a);
+
+  var b = 20;
+  print(b);
+
+  // final 只能被赋值一次
+  final c = 30;
+//  c = 50;
+
+  // 常量只能被声明一次
+  const d = 40;
+//  d = 40;
+}
