8d8b55d60481a3f447b9587825cb668600f33bcb
Jaden Dong <app@jadenmail.com>
2019-03-31 03:04:42 +0800

    算术运算符

---
+++ ch3/operator_arithmetic.dart
@@ -0,0 +1,17 @@
+void main() {
+  int a = 10;
+  int b = 2;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a ~/ b); // 取整
+  print(a % b); // 取余
+
+  print(a++); // 先打印再加1
+  print(++a); // 先加1再打印
+
+  print(a--);
+  print(--a);
+}
