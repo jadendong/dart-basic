641f6946e47bc235cf094fc92e92459b72418c18
Jaden Dong <app@jadenmail.com>
2019-03-31 03:14:50 +0800

    关系运算符

---
+++ ch3/operator_relational.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 5;
+  int b = 3;
+
+  print(a == b);
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+
+  String strA = "123";
+  String StrB = "321";
+  print(strA == StrB); // 判断内容是否相等
+}
